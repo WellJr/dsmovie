9be188978d919f6e6ef093e4db58330360d3d567
Wellington Junior <wellington.junior@sonda.com>
2022-01-17 19:13:28 -0300

    Corrigindo nome no Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/WellJr/dsmovie">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devesuperior</p>
+                            <p className="dsmovie-contact-link">/devsuperior</p>
                         </div>
                     </a>
                 </div>
